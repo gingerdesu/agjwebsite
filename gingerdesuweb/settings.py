19f19ae9b89737ce7eda72fa6bd350a97e738cd8
Annie Jiang <gingerdesu@gmail.com>
2018-10-28 19:48:47 -0500

    uploaded assests

--- gingerdesuweb/settings.py
+++ gingerdesuweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["agj-website.appspot.com", "localhost"]
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
